f5e6b00ae7e516e2af2985fd2dd9b81928fe0026
Viktor Matsiuk <mviktorfier@ukr.net>
2023-11-29 11:25:36 +0200

    Start commit

---
+++ src/main/java/ToPizza/com/ToPizzaApplication.java
@@ -0,0 +1,13 @@
+package ToPizza.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ToPizza/com/ToPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package ToPizza.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
