f82050a94acc92b32c860721589f512934b6d633
Viktor Matsiuk <mviktorfier@ukr.net>
2023-11-29 12:24:53 +0200

    site swich

--- src/main/java/ToPizza/com/HomeController.java
+++ src/main/java/ToPizza/com/HomeController.java
@@ -3,13 +3,25 @@ package ToPizza.com;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Hello, Spring Boot with Thymeleaf!");
-        return "index";
+        model.addAttribute("message", "Welcome to the Home Page!");
+        return "home";
+    }
+
+    @GetMapping("/other")
+    public String otherPage(Model model) {
+        model.addAttribute("message", "This is the Other Page!");
+        return "other";
+    }
+
+    @PostMapping("/submit")
+    public String submit() {
+        return "redirect:/other";
     }
 }
